6facecebc4cc82d02fbb660c333147c39dacab4b
Everton <everton.araujogta@gmail.com>
2024-04-03 08:34:45 -0300

    Criação do model e configuração do banco de dados

---
+++ app/Models/Models/ModelProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelProduct extends Model
+{
+    protected $table = 'product';
+}
